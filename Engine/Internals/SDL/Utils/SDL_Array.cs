fb735e73291d8d263a12f25c0c85294f0a385799
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-07 10:09:52 +0100

    Bindings

---
+++ Engine/Internals/SDL/Utils/SDL_Array.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+namespace Engine.SDL3
+{
+    internal static unsafe partial class SDL
+    {
+        public static T[] SDL_PointerToArray<T>(T* ptr, out int count, bool free = true) where T : unmanaged
+        {
+            count = 0;
+
+            if (ptr == null)
+            {
+                return Array.Empty<T>();
+            }
+
+            try
+            {
+                while (!EqualityComparer<T>.Default.Equals(ptr[count], default(T)))
+                {
+                    count++;
+                }
+
+                return new ReadOnlySpan<T>(ptr, count).ToArray();
+            }
+            finally
+            {
+                if (free)
+                {
+                    iSDL_free(ptr);
+                }
+            }
+        }
+        
+        public static T[] SDL_PointerToArray<T>(T* ptr, bool free = true) where T : unmanaged
+        {
+            int count = 0;
+
+            if (ptr == null)
+            {
+                return Array.Empty<T>();
+            }
+
+            try
+            {
+                while (!EqualityComparer<T>.Default.Equals(ptr[count], default(T)))
+                {
+                    count++;
+                }
+
+                return new ReadOnlySpan<T>(ptr, count).ToArray();
+            }
+            finally
+            {
+                if (free)
+                {
+                    iSDL_free(ptr);
+                }
+            }
+        }
+    }
+}
