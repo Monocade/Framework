71bc58d0b1c32fae860635e8f192a5a4486f1250
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-07 10:20:45 +0100

    Update SDL_assert.cs

--- Engine/Internals/SDL/Classes/SDL_assert.cs
+++ Engine/Internals/SDL/Classes/SDL_assert.cs
@@ -5,29 +5,37 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static SDL_AssertState SDL_ReportAssertion(SDL_AssertData* data, byte* func, byte* file, int line)
+        public static SDL_AssertState SDL_ReportAssertion(ref SDL_AssertData data, string func, string file, int line)
         {
-            return iSDL_ReportAssertion(data, func, file, line);
+            fixed (SDL_AssertData* ptr1 = &data)
+            fixed (byte* ptr2 = SDL_StringToUtf8(func))
+            fixed (byte* ptr3 = SDL_StringToUtf8(file))
+            {
+                return iSDL_ReportAssertion(ptr1, ptr2, ptr3, line);
+            }
         }
 
-        public static void SDL_SetAssertionHandler(IntPtr handler, void* userdata)
+        public static void SDL_SetAssertionHandler(SDL_AssertionHandler handler, IntPtr userdata)
         {
-            iSDL_SetAssertionHandler(handler, userdata);
+            iSDL_SetAssertionHandler(Marshal.GetFunctionPointerForDelegate(handler), (void*)userdata);
         }
 
-        public static IntPtr SDL_GetDefaultAssertionHandler()
+        public static SDL_AssertionHandler SDL_GetDefaultAssertionHandler()
         {
-            return iSDL_GetDefaultAssertionHandler();
+            return Marshal.GetDelegateForFunctionPointer<SDL_AssertionHandler>(iSDL_GetDefaultAssertionHandler());
         }
 
-        public static IntPtr SDL_GetAssertionHandler(void** puserdata)
+        public static SDL_AssertionHandler SDL_GetAssertionHandler(out IntPtr puserdata)
         {
-            return iSDL_GetAssertionHandler(puserdata);
+            fixed (IntPtr* ptr1 = &puserdata)
+            {
+                return Marshal.GetDelegateForFunctionPointer<SDL_AssertionHandler>(iSDL_GetAssertionHandler((void**)ptr1));
+            }
         }
 
-        public static SDL_AssertData* SDL_GetAssertionReport()
+        public static SDL_AssertData SDL_GetAssertionReport()
         {
-            return iSDL_GetAssertionReport();
+            return *iSDL_GetAssertionReport();
         }
 
         public static void SDL_ResetAssertionReport()
