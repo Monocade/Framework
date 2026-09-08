a49a3d34160c6df4f08ff7fee716dd3c94378ebb
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-08 14:30:28 +0100

    Update SDL_gamepad.cs

--- Engine/Internals/SDL/Classes/SDL_gamepad.cs
+++ Engine/Internals/SDL/Classes/SDL_gamepad.cs
@@ -5,69 +5,78 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static int SDL_AddGamepadMapping(byte* mapping)
+        public static int SDL_AddGamepadMapping(string mapping)
         {
-            return iSDL_AddGamepadMapping(mapping);
+            fixed (byte* ptr1 = SDL_StringToNative(mapping))
+            {
+                return iSDL_AddGamepadMapping(ptr1);
+            }
         }
 
-        public static int SDL_AddGamepadMappingsFromIO(SDL_IOStream* src, SDL_Bool closeio)
+        public static int SDL_AddGamepadMappingsFromIO(SDL_IOStream* src, bool closeio)
         {
             return iSDL_AddGamepadMappingsFromIO(src, closeio);
         }
 
-        public static int SDL_AddGamepadMappingsFromFile(byte* file)
+        public static int SDL_AddGamepadMappingsFromFile(string file)
         {
-            return iSDL_AddGamepadMappingsFromFile(file);
+            fixed (byte* ptr1 = SDL_StringToNative(file))
+            {
+                return iSDL_AddGamepadMappingsFromFile(ptr1);
+            }
         }
 
-        public static SDL_Bool SDL_ReloadGamepadMappings()
+        public static bool SDL_ReloadGamepadMappings()
         {
             return iSDL_ReloadGamepadMappings();
         }
 
-        public static byte** SDL_GetGamepadMappings(int* count)
+        public static string[] SDL_GetGamepadMappings(out int count)
         {
-            return iSDL_GetGamepadMappings(count);
+            return SDL_NativeToStringArray(iSDL_GetGamepadMappings(null), out count);
         }
 
-        public static byte* SDL_GetGamepadMappingForGUID(SDL_GUID guid)
+        public static string SDL_GetGamepadMappingForGUID(SDL_GUID guid)
         {
-            return iSDL_GetGamepadMappingForGUID(guid);
+            return SDL_NativeToString(iSDL_GetGamepadMappingForGUID(guid));
         }
 
-        public static byte* SDL_GetGamepadMapping(SDL_Gamepad* gamepad)
+        public static string SDL_GetGamepadMapping(SDL_Gamepad* gamepad)
         {
-            return iSDL_GetGamepadMapping(gamepad);
+            return SDL_NativeToString(iSDL_GetGamepadMapping(gamepad));
         }
 
-        public static SDL_Bool SDL_SetGamepadMapping(uint instance_id, byte* mapping)
+        public static bool SDL_SetGamepadMapping(uint instance_id, string mapping)
         {
-            return iSDL_SetGamepadMapping(instance_id, mapping);
+            fixed (byte* ptr1 = SDL_StringToNative(mapping))
+            {
+                return iSDL_SetGamepadMapping(instance_id, ptr1);
+            }
         }
 
-        public static SDL_Bool SDL_HasGamepad()
+        public static bool SDL_HasGamepad()
         {
             return iSDL_HasGamepad();
         }
 
-        public static uint* SDL_GetGamepads(int* count)
+        public static uint[] SDL_GetGamepads(out int count)
         {
-            return iSDL_GetGamepads(count);
+            return SDL_NativeToArray(iSDL_GetGamepads(null), out count);
         }
 
-        public static SDL_Bool SDL_IsGamepad(uint instance_id)
+        public static bool SDL_IsGamepad(uint instance_id)
         {
             return iSDL_IsGamepad(instance_id);
         }
 
-        public static byte* SDL_GetGamepadNameForID(uint instance_id)
+        public static string SDL_GetGamepadNameForID(uint instance_id)
         {
-            return iSDL_GetGamepadNameForID(instance_id);
+            return SDL_NativeToString(iSDL_GetGamepadNameForID(instance_id));
         }
 
-        public static byte* SDL_GetGamepadPathForID(uint instance_id)
+        public static string SDL_GetGamepadPathForID(uint instance_id)
         {
-            return iSDL_GetGamepadPathForID(instance_id);
+            return SDL_NativeToString(iSDL_GetGamepadPathForID(instance_id));
         }
 
         public static int SDL_GetGamepadPlayerIndexForID(uint instance_id)
@@ -105,9 +114,9 @@ namespace Engine.SDL3
             return iSDL_GetRealGamepadTypeForID(instance_id);
         }
 
-        public static byte* SDL_GetGamepadMappingForID(uint instance_id)
+        public static string SDL_GetGamepadMappingForID(uint instance_id)
         {
-            return iSDL_GetGamepadMappingForID(instance_id);
+            return SDL_NativeToString(iSDL_GetGamepadMappingForID(instance_id));
         }
 
         public static SDL_Gamepad* SDL_OpenGamepad(uint instance_id)
@@ -135,14 +144,14 @@ namespace Engine.SDL3
             return iSDL_GetGamepadID(gamepad);
         }
 
-        public static byte* SDL_GetGamepadName(SDL_Gamepad* gamepad)
+        public static string SDL_GetGamepadName(SDL_Gamepad* gamepad)
         {
-            return iSDL_GetGamepadName(gamepad);
+            return SDL_NativeToString(iSDL_GetGamepadName(gamepad));
         }
 
-        public static byte* SDL_GetGamepadPath(SDL_Gamepad* gamepad)
+        public static string SDL_GetGamepadPath(SDL_Gamepad* gamepad)
         {
-            return iSDL_GetGamepadPath(gamepad);
+            return SDL_NativeToString(iSDL_GetGamepadPath(gamepad));
         }
 
         public static SDL_GamepadType SDL_GetGamepadType(SDL_Gamepad* gamepad)
@@ -160,7 +169,7 @@ namespace Engine.SDL3
             return iSDL_GetGamepadPlayerIndex(gamepad);
         }
 
-        public static SDL_Bool SDL_SetGamepadPlayerIndex(SDL_Gamepad* gamepad, int player_index)
+        public static bool SDL_SetGamepadPlayerIndex(SDL_Gamepad* gamepad, int player_index)
         {
             return iSDL_SetGamepadPlayerIndex(gamepad, player_index);
         }
@@ -185,9 +194,9 @@ namespace Engine.SDL3
             return iSDL_GetGamepadFirmwareVersion(gamepad);
         }
 
-        public static byte* SDL_GetGamepadSerial(SDL_Gamepad* gamepad)
+        public static string SDL_GetGamepadSerial(SDL_Gamepad* gamepad)
         {
-            return iSDL_GetGamepadSerial(gamepad);
+            return SDL_NativeToString(iSDL_GetGamepadSerial(gamepad));
         }
 
         public static ulong SDL_GetGamepadSteamHandle(SDL_Gamepad* gamepad)
@@ -200,12 +209,15 @@ namespace Engine.SDL3
             return iSDL_GetGamepadConnectionState(gamepad);
         }
 
-        public static SDL_PowerState SDL_GetGamepadPowerInfo(SDL_Gamepad* gamepad, int* percent)
+        public static SDL_PowerState SDL_GetGamepadPowerInfo(SDL_Gamepad* gamepad, out int percent)
         {
-            return iSDL_GetGamepadPowerInfo(gamepad, percent);
+            fixed (int* ptr1 = &percent)
+            {
+                return iSDL_GetGamepadPowerInfo(gamepad, ptr1);
+            }
         }
 
-        public static SDL_Bool SDL_GamepadConnected(SDL_Gamepad* gamepad)
+        public static bool SDL_GamepadConnected(SDL_Gamepad* gamepad)
         {
             return iSDL_GamepadConnected(gamepad);
         }
@@ -215,19 +227,19 @@ namespace Engine.SDL3
             return iSDL_GetGamepadJoystick(gamepad);
         }
 
-        public static void SDL_SetGamepadEventsEnabled(SDL_Bool enabled)
+        public static void SDL_SetGamepadEventsEnabled(bool enabled)
         {
             iSDL_SetGamepadEventsEnabled(enabled);
         }
 
-        public static SDL_Bool SDL_GamepadEventsEnabled()
+        public static bool SDL_GamepadEventsEnabled()
         {
             return iSDL_GamepadEventsEnabled();
         }
 
-        public static SDL_GamepadBinding** SDL_GetGamepadBindings(SDL_Gamepad* gamepad, int* count)
+        public static SDL_GamepadBinding*[] SDL_GetGamepadBindings(SDL_Gamepad* gamepad, out int count)
         {
-            return iSDL_GetGamepadBindings(gamepad, count);
+            return SDL_NativeToArray(iSDL_GetGamepadBindings(gamepad, null), out count);
         }
 
         public static void SDL_UpdateGamepads()
@@ -235,27 +247,33 @@ namespace Engine.SDL3
             iSDL_UpdateGamepads();
         }
 
-        public static SDL_GamepadType SDL_GetGamepadTypeFromString(byte* str)
+        public static SDL_GamepadType SDL_GetGamepadTypeFromString(string str)
         {
-            return iSDL_GetGamepadTypeFromString(str);
+            fixed (byte* ptr1 = SDL_StringToNative(str))
+            {
+                return iSDL_GetGamepadTypeFromString(ptr1);
+            }
         }
 
-        public static byte* SDL_GetGamepadStringForType(SDL_GamepadType type)
+        public static string SDL_GetGamepadStringForType(SDL_GamepadType type)
         {
-            return iSDL_GetGamepadStringForType(type);
+            return SDL_NativeToString(iSDL_GetGamepadStringForType(type));
         }
 
-        public static SDL_GamepadAxis SDL_GetGamepadAxisFromString(byte* str)
+        public static SDL_GamepadAxis SDL_GetGamepadAxisFromString(string str)
         {
-            return iSDL_GetGamepadAxisFromString(str);
+            fixed (byte* ptr1 = SDL_StringToNative(str))
+            {
+                return iSDL_GetGamepadAxisFromString(ptr1);
+            }
         }
 
-        public static byte* SDL_GetGamepadStringForAxis(SDL_GamepadAxis axis)
+        public static string SDL_GetGamepadStringForAxis(SDL_GamepadAxis axis)
         {
-            return iSDL_GetGamepadStringForAxis(axis);
+            return SDL_NativeToString(iSDL_GetGamepadStringForAxis(axis));
         }
 
-        public static SDL_Bool SDL_GamepadHasAxis(SDL_Gamepad* gamepad, SDL_GamepadAxis axis)
+        public static bool SDL_GamepadHasAxis(SDL_Gamepad* gamepad, SDL_GamepadAxis axis)
         {
             return iSDL_GamepadHasAxis(gamepad, axis);
         }
@@ -265,22 +283,25 @@ namespace Engine.SDL3
             return iSDL_GetGamepadAxis(gamepad, axis);
         }
 
-        public static SDL_GamepadButton SDL_GetGamepadButtonFromString(byte* str)
+        public static SDL_GamepadButton SDL_GetGamepadButtonFromString(string str)
         {
-            return iSDL_GetGamepadButtonFromString(str);
+            fixed (byte* ptr1 = SDL_StringToNative(str))
+            {
+                return iSDL_GetGamepadButtonFromString(ptr1);
+            }
         }
 
-        public static byte* SDL_GetGamepadStringForButton(SDL_GamepadButton button)
+        public static string SDL_GetGamepadStringForButton(SDL_GamepadButton button)
         {
-            return iSDL_GetGamepadStringForButton(button);
+            return SDL_NativeToString(iSDL_GetGamepadStringForButton(button));
         }
 
-        public static SDL_Bool SDL_GamepadHasButton(SDL_Gamepad* gamepad, SDL_GamepadButton button)
+        public static bool SDL_GamepadHasButton(SDL_Gamepad* gamepad, SDL_GamepadButton button)
         {
             return iSDL_GamepadHasButton(gamepad, button);
         }
 
-        public static SDL_Bool SDL_GetGamepadButton(SDL_Gamepad* gamepad, SDL_GamepadButton button)
+        public static bool SDL_GetGamepadButton(SDL_Gamepad* gamepad, SDL_GamepadButton button)
         {
             return iSDL_GetGamepadButton(gamepad, button);
         }
@@ -305,22 +326,28 @@ namespace Engine.SDL3
             return iSDL_GetNumGamepadTouchpadFingers(gamepad, touchpad);
         }
 
-        public static SDL_Bool SDL_GetGamepadTouchpadFinger(SDL_Gamepad* gamepad, int touchpad, int finger, SDL_Bool* down, float* x, float* y, float* pressure)
+        public static bool SDL_GetGamepadTouchpadFinger(SDL_Gamepad* gamepad, int touchpad, int finger, out SDL_Bool down, out float x, out float y, out float pressure)
         {
-            return iSDL_GetGamepadTouchpadFinger(gamepad, touchpad, finger, down, x, y, pressure);
+            fixed (SDL_Bool* ptr1 = &down)
+            fixed (float* ptr2 = &x)
+            fixed (float* ptr3 = &y)
+            fixed (float* ptr4 = &pressure)
+            {
+                return iSDL_GetGamepadTouchpadFinger(gamepad, touchpad, finger, ptr1, ptr2, ptr3, ptr4);
+            }
         }
 
-        public static SDL_Bool SDL_GamepadHasSensor(SDL_Gamepad* gamepad, SDL_SensorType type)
+        public static bool SDL_GamepadHasSensor(SDL_Gamepad* gamepad, SDL_SensorType type)
         {
             return iSDL_GamepadHasSensor(gamepad, type);
         }
 
-        public static SDL_Bool SDL_SetGamepadSensorEnabled(SDL_Gamepad* gamepad, SDL_SensorType type, SDL_Bool enabled)
+        public static bool SDL_SetGamepadSensorEnabled(SDL_Gamepad* gamepad, SDL_SensorType type, bool enabled)
         {
             return iSDL_SetGamepadSensorEnabled(gamepad, type, enabled);
         }
 
-        public static SDL_Bool SDL_GamepadSensorEnabled(SDL_Gamepad* gamepad, SDL_SensorType type)
+        public static bool SDL_GamepadSensorEnabled(SDL_Gamepad* gamepad, SDL_SensorType type)
         {
             return iSDL_GamepadSensorEnabled(gamepad, type);
         }
@@ -330,37 +357,43 @@ namespace Engine.SDL3
             return iSDL_GetGamepadSensorDataRate(gamepad, type);
         }
 
-        public static SDL_Bool SDL_GetGamepadSensorData(SDL_Gamepad* gamepad, SDL_SensorType type, float* data, int num_values)
+        public static bool SDL_GetGamepadSensorData(SDL_Gamepad* gamepad, SDL_SensorType type, out float[] data, int num_values)
         {
-            return iSDL_GetGamepadSensorData(gamepad, type, data, num_values);
+            data = new float[num_values];
+            {
+                fixed (float* ptr1 = data)
+                {
+                    return iSDL_GetGamepadSensorData(gamepad, type, ptr1, num_values);
+                }
+            }
         }
 
-        public static SDL_Bool SDL_GamepadHasCapSense(SDL_Gamepad* gamepad, SDL_GamepadCapSenseType type)
+        public static bool SDL_GamepadHasCapSense(SDL_Gamepad* gamepad, SDL_GamepadCapSenseType type)
         {
             return iSDL_GamepadHasCapSense(gamepad, type);
         }
 
-        public static SDL_Bool SDL_GetGamepadCapSense(SDL_Gamepad* gamepad, SDL_GamepadCapSenseType type)
+        public static bool SDL_GetGamepadCapSense(SDL_Gamepad* gamepad, SDL_GamepadCapSenseType type)
         {
             return iSDL_GetGamepadCapSense(gamepad, type);
         }
 
-        public static SDL_Bool SDL_RumbleGamepad(SDL_Gamepad* gamepad, ushort low_frequency_rumble, ushort high_frequency_rumble, uint duration_ms)
+        public static bool SDL_RumbleGamepad(SDL_Gamepad* gamepad, ushort low_frequency_rumble, ushort high_frequency_rumble, uint duration_ms)
         {
             return iSDL_RumbleGamepad(gamepad, low_frequency_rumble, high_frequency_rumble, duration_ms);
         }
 
-        public static SDL_Bool SDL_RumbleGamepadTriggers(SDL_Gamepad* gamepad, ushort left_rumble, ushort right_rumble, uint duration_ms)
+        public static bool SDL_RumbleGamepadTriggers(SDL_Gamepad* gamepad, ushort left_rumble, ushort right_rumble, uint duration_ms)
         {
             return iSDL_RumbleGamepadTriggers(gamepad, left_rumble, right_rumble, duration_ms);
         }
 
-        public static SDL_Bool SDL_SetGamepadLED(SDL_Gamepad* gamepad, byte red, byte green, byte blue)
+        public static bool SDL_SetGamepadLED(SDL_Gamepad* gamepad, byte red, byte green, byte blue)
         {
             return iSDL_SetGamepadLED(gamepad, red, green, blue);
         }
 
-        public static SDL_Bool SDL_SendGamepadEffect(SDL_Gamepad* gamepad, IntPtr data, int size)
+        public static bool SDL_SendGamepadEffect(SDL_Gamepad* gamepad, IntPtr data, int size)
         {
             return iSDL_SendGamepadEffect(gamepad, data, size);
         }
@@ -370,14 +403,14 @@ namespace Engine.SDL3
             iSDL_CloseGamepad(gamepad);
         }
 
-        public static byte* SDL_GetGamepadAppleSFSymbolsNameForButton(SDL_Gamepad* gamepad, SDL_GamepadButton button)
+        public static string SDL_GetGamepadAppleSFSymbolsNameForButton(SDL_Gamepad* gamepad, SDL_GamepadButton button)
         {
-            return iSDL_GetGamepadAppleSFSymbolsNameForButton(gamepad, button);
+            return SDL_NativeToString(iSDL_GetGamepadAppleSFSymbolsNameForButton(gamepad, button));
         }
 
-        public static byte* SDL_GetGamepadAppleSFSymbolsNameForAxis(SDL_Gamepad* gamepad, SDL_GamepadAxis axis)
+        public static string SDL_GetGamepadAppleSFSymbolsNameForAxis(SDL_Gamepad* gamepad, SDL_GamepadAxis axis)
         {
-            return iSDL_GetGamepadAppleSFSymbolsNameForAxis(gamepad, axis);
+            return SDL_NativeToString(iSDL_GetGamepadAppleSFSymbolsNameForAxis(gamepad, axis));
         }
     }
 }
