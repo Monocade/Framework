05bb883a0b7e20520342a52ee2825d837b3a11ee
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-08 14:47:38 +0100

    Update SDL_guid.cs

--- Engine/Internals/SDL/Classes/SDL_guid.cs
+++ Engine/Internals/SDL/Classes/SDL_guid.cs
@@ -5,14 +5,20 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static void SDL_GUIDToString(SDL_GUID guid, byte* pszGUID, int cbGUID)
+        public static void SDL_GUIDToString(SDL_GUID guid, string pszGUID, int cbGUID)
         {
-            iSDL_GUIDToString(guid, pszGUID, cbGUID);
+            fixed (byte* ptr1 = SDL_StringToNative(pszGUID))
+            {
+                iSDL_GUIDToString(guid, ptr1, cbGUID);
+            }
         }
 
-        public static SDL_GUID SDL_StringToGUID(byte* pchGUID)
+        public static SDL_GUID SDL_StringToGUID(string pchGUID)
         {
-            return iSDL_StringToGUID(pchGUID);
+            fixed (byte* ptr1 = SDL_StringToNative(pchGUID))
+            {
+                return iSDL_StringToGUID(ptr1);
+            }
         }
     }
 }
