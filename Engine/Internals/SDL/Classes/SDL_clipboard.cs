d48662dd0bf2f289398546f39139b84e2956b1ae
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-08 14:04:41 +0100

    Update SDL_clipboard.cs

--- Engine/Internals/SDL/Classes/SDL_clipboard.cs
+++ Engine/Internals/SDL/Classes/SDL_clipboard.cs
@@ -5,59 +5,75 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static SDL_Bool SDL_SetClipboardText(byte* text)
+        public static bool SDL_SetClipboardText(string text)
         {
-            return iSDL_SetClipboardText(text);
+            fixed (byte* ptr1 = SDL_StringToNative(text))
+            {
+                return iSDL_SetClipboardText(ptr1);
+            }
         }
 
-        public static byte* SDL_GetClipboardText()
+        public static string SDL_GetClipboardText()
         {
-            return iSDL_GetClipboardText();
+            return SDL_NativeToString(iSDL_GetClipboardText(), true);
         }
 
-        public static SDL_Bool SDL_HasClipboardText()
+        public static bool SDL_HasClipboardText()
         {
             return iSDL_HasClipboardText();
         }
 
-        public static SDL_Bool SDL_SetPrimarySelectionText(byte* text)
+        public static bool SDL_SetPrimarySelectionText(string text)
         {
-            return iSDL_SetPrimarySelectionText(text);
+            fixed (byte* ptr1 = SDL_StringToNative(text))
+            {
+                return iSDL_SetPrimarySelectionText(ptr1);
+            }
         }
 
-        public static byte* SDL_GetPrimarySelectionText()
+        public static string SDL_GetPrimarySelectionText()
         {
-            return iSDL_GetPrimarySelectionText();
+            return SDL_NativeToString(iSDL_GetPrimarySelectionText(), true);
         }
 
-        public static SDL_Bool SDL_HasPrimarySelectionText()
+        public static bool SDL_HasPrimarySelectionText()
         {
             return iSDL_HasPrimarySelectionText();
         }
 
-        public static SDL_Bool SDL_SetClipboardData(IntPtr callback, IntPtr cleanup, IntPtr userdata, byte** mime_types, nuint num_mime_types)
+        public static bool SDL_SetClipboardData(SDL_ClipboardDataCallback callback, SDL_ClipboardCleanupCallback cleanup, IntPtr userdata, IntPtr mime_types, UIntPtr num_mime_types)
         {
-            return iSDL_SetClipboardData(callback, cleanup, userdata, mime_types, num_mime_types);
+            return iSDL_SetClipboardData(Marshal.GetFunctionPointerForDelegate(callback), Marshal.GetFunctionPointerForDelegate(cleanup), userdata, (byte**)mime_types, num_mime_types);
         }
 
-        public static SDL_Bool SDL_ClearClipboardData()
+        public static bool SDL_ClearClipboardData()
         {
             return iSDL_ClearClipboardData();
         }
 
-        public static IntPtr SDL_GetClipboardData(byte* mime_type, nuint* size)
+        public static IntPtr SDL_GetClipboardData(string mime_type, out UIntPtr size)
         {
-            return iSDL_GetClipboardData(mime_type, size);
+            fixed (byte* ptr1 = SDL_StringToNative(mime_type))
+            fixed (nuint* ptr2 = &size)
+            {
+                return iSDL_GetClipboardData(ptr1, ptr2);
+            }
         }
 
-        public static SDL_Bool SDL_HasClipboardData(byte* mime_type)
+        public static bool SDL_HasClipboardData(string mime_type)
         {
-            return iSDL_HasClipboardData(mime_type);
+            fixed (byte* ptr1 = SDL_StringToNative(mime_type))
+            {
+                return iSDL_HasClipboardData(ptr1);
+            }
         }
 
-        public static byte** SDL_GetClipboardMimeTypes(nuint* num_mime_types)
+        public static IntPtr SDL_GetClipboardMimeTypes(out UIntPtr num_mime_types)
         {
-            return iSDL_GetClipboardMimeTypes(num_mime_types);
+            fixed (nuint* ptr1 = &num_mime_types)
+            {
+                return (IntPtr)iSDL_GetClipboardMimeTypes(ptr1);
+            }
         }
     }
 }
