7dcc54ce959eadf19aa2d87101f7e4f4eb4b1017
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-08 15:32:55 +0100

    Update SDL_mouse.cs

--- Engine/Internals/SDL/Classes/SDL_mouse.cs
+++ Engine/Internals/SDL/Classes/SDL_mouse.cs
@@ -5,19 +5,19 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static SDL_Bool SDL_HasMouse()
+        public static bool SDL_HasMouse()
         {
             return iSDL_HasMouse();
         }
 
-        public static uint* SDL_GetMice(int* count)
+        public static uint[] SDL_GetMice(out int count)
         {
-            return iSDL_GetMice(count);
+            return SDL_NativeToArray(iSDL_GetMice(null), out count);
         }
 
-        public static byte* SDL_GetMouseNameForID(uint instance_id)
+        public static string SDL_GetMouseNameForID(uint instance_id)
         {
-            return iSDL_GetMouseNameForID(instance_id);
+            return SDL_NativeToString(iSDL_GetMouseNameForID(instance_id));
         }
 
         public static SDL_Window* SDL_GetMouseFocus()
@@ -25,19 +25,31 @@ namespace Engine.SDL3
             return iSDL_GetMouseFocus();
         }
 
-        public static uint SDL_GetMouseState(float* x, float* y)
+        public static uint SDL_GetMouseState(out float x, out float y)
         {
-            return iSDL_GetMouseState(x, y);
+            fixed (float* ptr1 = &x)
+            fixed (float* ptr2 = &y)
+            {
+                return iSDL_GetMouseState(ptr1, ptr2);
+            }
         }
 
-        public static uint SDL_GetGlobalMouseState(float* x, float* y)
+        public static uint SDL_GetGlobalMouseState(out float x, out float y)
         {
-            return iSDL_GetGlobalMouseState(x, y);
+            fixed (float* ptr1 = &x)
+            fixed (float* ptr2 = &y)
+            {
+                return iSDL_GetGlobalMouseState(ptr1, ptr2);
+            }
         }
 
-        public static uint SDL_GetRelativeMouseState(float* x, float* y)
+        public static uint SDL_GetRelativeMouseState(out float x, out float y)
         {
-            return iSDL_GetRelativeMouseState(x, y);
+            fixed (float* ptr1 = &x)
+            fixed (float* ptr2 = &y)
+            {
+                return iSDL_GetRelativeMouseState(ptr1, ptr2);
+            }
         }
 
         public static void SDL_WarpMouseInWindow(SDL_Window* window, float x, float y)
@@ -45,34 +57,34 @@ namespace Engine.SDL3
             iSDL_WarpMouseInWindow(window, x, y);
         }
 
-        public static SDL_Bool SDL_WarpMouseGlobal(float x, float y)
+        public static bool SDL_WarpMouseGlobal(float x, float y)
         {
             return iSDL_WarpMouseGlobal(x, y);
         }
 
-        public static SDL_Bool SDL_SetRelativeMouseTransform(IntPtr callback, IntPtr userdata)
+        public static bool SDL_SetRelativeMouseTransform(SDL_MouseMotionTransformCallback callback, IntPtr userdata)
         {
-            return iSDL_SetRelativeMouseTransform(callback, userdata);
+            return iSDL_SetRelativeMouseTransform(Marshal.GetFunctionPointerForDelegate(callback), userdata);
         }
 
-        public static SDL_Bool SDL_SetWindowRelativeMouseMode(SDL_Window* window, SDL_Bool enabled)
+        public static bool SDL_SetWindowRelativeMouseMode(SDL_Window* window, bool enabled)
         {
             return iSDL_SetWindowRelativeMouseMode(window, enabled);
         }
 
-        public static SDL_Bool SDL_GetWindowRelativeMouseMode(SDL_Window* window)
+        public static bool SDL_GetWindowRelativeMouseMode(SDL_Window* window)
         {
             return iSDL_GetWindowRelativeMouseMode(window);
         }
 
-        public static SDL_Bool SDL_CaptureMouse(SDL_Bool enabled)
+        public static bool SDL_CaptureMouse(bool enabled)
         {
             return iSDL_CaptureMouse(enabled);
         }
 
-        public static SDL_Cursor* SDL_CreateCursor(byte* data, byte* mask, int w, int h, int hot_x, int hot_y)
+        public static SDL_Cursor* SDL_CreateCursor(IntPtr data, IntPtr mask, int w, int h, int hot_x, int hot_y)
         {
-            return iSDL_CreateCursor(data, mask, w, h, hot_x, hot_y);
+            return iSDL_CreateCursor((byte*)data, (byte*)mask, w, h, hot_x, hot_y);
         }
 
         public static SDL_Cursor* SDL_CreateColorCursor(SDL_Surface* surface, int hot_x, int hot_y)
@@ -80,9 +92,12 @@ namespace Engine.SDL3
             return iSDL_CreateColorCursor(surface, hot_x, hot_y);
         }
 
-        public static SDL_Cursor* SDL_CreateAnimatedCursor(SDL_CursorFrameInfo* frames, int frame_count, int hot_x, int hot_y)
+        public static SDL_Cursor* SDL_CreateAnimatedCursor(SDL_CursorFrameInfo[] frames, int frame_count, int hot_x, int hot_y)
         {
-            return iSDL_CreateAnimatedCursor(frames, frame_count, hot_x, hot_y);
+            fixed (SDL_CursorFrameInfo* ptr1 = frames)
+            {
+                return iSDL_CreateAnimatedCursor(ptr1, frame_count, hot_x, hot_y);
+            }
         }
 
         public static SDL_Cursor* SDL_CreateSystemCursor(SDL_SystemCursor id)
@@ -90,7 +105,7 @@ namespace Engine.SDL3
             return iSDL_CreateSystemCursor(id);
         }
 
-        public static SDL_Bool SDL_SetCursor(SDL_Cursor* cursor)
+        public static bool SDL_SetCursor(SDL_Cursor* cursor)
         {
             return iSDL_SetCursor(cursor);
         }
@@ -110,17 +125,17 @@ namespace Engine.SDL3
             iSDL_DestroyCursor(cursor);
         }
 
-        public static SDL_Bool SDL_ShowCursor()
+        public static bool SDL_ShowCursor()
         {
             return iSDL_ShowCursor();
         }
 
-        public static SDL_Bool SDL_HideCursor()
+        public static bool SDL_HideCursor()
         {
             return iSDL_HideCursor();
         }
 
-        public static SDL_Bool SDL_CursorVisible()
+        public static bool SDL_CursorVisible()
         {
             return iSDL_CursorVisible();
         }
