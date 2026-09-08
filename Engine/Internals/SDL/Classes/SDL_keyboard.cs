3038041c86a6b512780e14439e54598b33dc0a6c
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-08 15:17:08 +0100

    Update SDL_keyboard.cs

--- Engine/Internals/SDL/Classes/SDL_keyboard.cs
+++ Engine/Internals/SDL/Classes/SDL_keyboard.cs
@@ -5,19 +5,19 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static SDL_Bool SDL_HasKeyboard()
+        public static bool SDL_HasKeyboard()
         {
             return iSDL_HasKeyboard();
         }
 
-        public static uint* SDL_GetKeyboards(int* count)
+        public static uint[] SDL_GetKeyboards(out int count)
         {
-            return iSDL_GetKeyboards(count);
+            return SDL_NativeToArray(iSDL_GetKeyboards(null), out count);
         }
 
-        public static byte* SDL_GetKeyboardNameForID(uint instance_id)
+        public static string SDL_GetKeyboardNameForID(uint instance_id)
         {
-            return iSDL_GetKeyboardNameForID(instance_id);
+            return SDL_NativeToString(iSDL_GetKeyboardNameForID(instance_id));
         }
 
         public static SDL_Window* SDL_GetKeyboardFocus()
@@ -25,9 +25,9 @@ namespace Engine.SDL3
             return iSDL_GetKeyboardFocus();
         }
 
-        public static SDL_Bool* SDL_GetKeyboardState(int* numkeys)
+        public static SDL_Bool[] SDL_GetKeyboardState(out int numkeys)
         {
-            return iSDL_GetKeyboardState(numkeys);
+            return SDL_NativeToArray(iSDL_GetKeyboardState(null), out numkeys);
         }
 
         public static void SDL_ResetKeyboard()
@@ -45,82 +45,98 @@ namespace Engine.SDL3
             iSDL_SetModState(modstate);
         }
 
-        public static uint SDL_GetKeyFromScancode(SDL_Scancode scancode, ushort modstate, SDL_Bool key_event)
+        public static uint SDL_GetKeyFromScancode(SDL_Scancode scancode, ushort modstate, bool key_event)
         {
             return iSDL_GetKeyFromScancode(scancode, modstate, key_event);
         }
 
-        public static SDL_Scancode SDL_GetScancodeFromKey(uint key, ushort* modstate)
+        public static SDL_Scancode SDL_GetScancodeFromKey(uint key, ref ushort modstate)
         {
-            return iSDL_GetScancodeFromKey(key, modstate);
+            fixed (ushort* ptr1 = &modstate)
+            {
+                return iSDL_GetScancodeFromKey(key, ptr1);
+            }
         }
 
-        public static SDL_Bool SDL_SetScancodeName(SDL_Scancode scancode, byte* name)
+        public static bool SDL_SetScancodeName(SDL_Scancode scancode, string name)
         {
-            return iSDL_SetScancodeName(scancode, name);
+            fixed (byte* ptr1 = SDL_StringToNative(name))
+            {
+                return iSDL_SetScancodeName(scancode, ptr1);
+            }
         }
 
-        public static byte* SDL_GetScancodeName(SDL_Scancode scancode)
+        public static string SDL_GetScancodeName(SDL_Scancode scancode)
         {
-            return iSDL_GetScancodeName(scancode);
+            return SDL_NativeToString(iSDL_GetScancodeName(scancode));
         }
 
-        public static SDL_Scancode SDL_GetScancodeFromName(byte* name)
+        public static SDL_Scancode SDL_GetScancodeFromName(string name)
         {
-            return iSDL_GetScancodeFromName(name);
+            fixed (byte* ptr1 = SDL_StringToNative(name))
+            {
+                return iSDL_GetScancodeFromName(ptr1);
+            }
         }
 
-        public static byte* SDL_GetKeyName(uint key)
+        public static string SDL_GetKeyName(uint key)
         {
-            return iSDL_GetKeyName(key);
+            return SDL_NativeToString(iSDL_GetKeyName(key));
         }
 
-        public static uint SDL_GetKeyFromName(byte* name)
+        public static uint SDL_GetKeyFromName(string name)
         {
-            return iSDL_GetKeyFromName(name);
+            fixed (byte* ptr1 = SDL_StringToNative(name))
+            {
+                return iSDL_GetKeyFromName(ptr1);
+            }
         }
 
-        public static SDL_Bool SDL_StartTextInput(SDL_Window* window)
+        public static bool SDL_StartTextInput(SDL_Window* window)
         {
             return iSDL_StartTextInput(window);
         }
 
-        public static SDL_Bool SDL_StartTextInputWithProperties(SDL_Window* window, uint props)
+        public static bool SDL_StartTextInputWithProperties(SDL_Window* window, uint props)
         {
             return iSDL_StartTextInputWithProperties(window, props);
         }
 
-        public static SDL_Bool SDL_TextInputActive(SDL_Window* window)
+        public static bool SDL_TextInputActive(SDL_Window* window)
         {
             return iSDL_TextInputActive(window);
         }
 
-        public static SDL_Bool SDL_StopTextInput(SDL_Window* window)
+        public static bool SDL_StopTextInput(SDL_Window* window)
         {
             return iSDL_StopTextInput(window);
         }
 
-        public static SDL_Bool SDL_ClearComposition(SDL_Window* window)
+        public static bool SDL_ClearComposition(SDL_Window* window)
         {
             return iSDL_ClearComposition(window);
         }
 
-        public static SDL_Bool SDL_SetTextInputArea(SDL_Window* window, SDL_Rect* rect, int cursor)
+        public static bool SDL_SetTextInputArea(SDL_Window* window, SDL_Rect* rect, int cursor)
         {
             return iSDL_SetTextInputArea(window, rect, cursor);
         }
 
-        public static SDL_Bool SDL_GetTextInputArea(SDL_Window* window, SDL_Rect* rect, int* cursor)
+        public static bool SDL_GetTextInputArea(SDL_Window* window, out SDL_Rect rect, out int cursor)
         {
-            return iSDL_GetTextInputArea(window, rect, cursor);
+            fixed (SDL_Rect* ptr1 = &rect)
+            fixed (int* ptr2 = &cursor)
+            {
+                return iSDL_GetTextInputArea(window, ptr1, ptr2);
+            }
         }
 
-        public static SDL_Bool SDL_HasScreenKeyboardSupport()
+        public static bool SDL_HasScreenKeyboardSupport()
         {
             return iSDL_HasScreenKeyboardSupport();
         }
 
-        public static SDL_Bool SDL_ScreenKeyboardShown(SDL_Window* window)
+        public static bool SDL_ScreenKeyboardShown(SDL_Window* window)
         {
             return iSDL_ScreenKeyboardShown(window);
         }
