d42b1e3cfb1ca43543280bdfb720451b36fdc7ac
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-08 15:29:27 +0100

    Update SDL_messagebox.cs

--- Engine/Internals/SDL/Classes/SDL_messagebox.cs
+++ Engine/Internals/SDL/Classes/SDL_messagebox.cs
@@ -5,14 +5,21 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static SDL_Bool SDL_ShowMessageBox(SDL_MessageBoxData* messageboxdata, int* buttonid)
+        public static bool SDL_ShowMessageBox(SDL_MessageBoxData* messageboxdata, out int buttonid)
         {
-            return iSDL_ShowMessageBox(messageboxdata, buttonid);
+            fixed (int* ptr1 = &buttonid)
+            {
+                return iSDL_ShowMessageBox(messageboxdata, ptr1);
+            }
         }
 
-        public static SDL_Bool SDL_ShowSimpleMessageBox(uint flags, byte* title, byte* message, SDL_Window* window)
+        public static bool SDL_ShowSimpleMessageBox(uint flags, string title, string message, SDL_Window* window)
         {
-            return iSDL_ShowSimpleMessageBox(flags, title, message, window);
+            fixed (byte* ptr1 = SDL_StringToNative(title))
+            fixed (byte* ptr2 = SDL_StringToNative(message))
+            {
+                return iSDL_ShowSimpleMessageBox(flags, ptr1, ptr2, window);
+            }
         }
     }
 }
