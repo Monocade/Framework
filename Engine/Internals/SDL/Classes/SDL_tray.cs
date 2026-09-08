1d491c8ea7703ab8984d8fc919dea9d1cedfcfac
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-08 16:47:31 +0100

    Update SDL_tray.cs

--- Engine/Internals/SDL/Classes/SDL_tray.cs
+++ Engine/Internals/SDL/Classes/SDL_tray.cs
@@ -5,9 +5,12 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static SDL_Tray* SDL_CreateTray(SDL_Surface* icon, byte* tooltip)
+        public static SDL_Tray* SDL_CreateTray(SDL_Surface* icon, string tooltip)
         {
-            return iSDL_CreateTray(icon, tooltip);
+            fixed (byte* ptr1 = SDL_StringToNative(tooltip))
+            {
+                return iSDL_CreateTray(icon, ptr1);
+            }
         }
 
         public static SDL_Tray* SDL_CreateTrayWithProperties(uint props)
@@ -20,9 +23,12 @@ namespace Engine.SDL3
             iSDL_SetTrayIcon(tray, icon);
         }
 
-        public static void SDL_SetTrayTooltip(SDL_Tray* tray, byte* tooltip)
+        public static void SDL_SetTrayTooltip(SDL_Tray* tray, string tooltip)
         {
-            iSDL_SetTrayTooltip(tray, tooltip);
+            fixed (byte* ptr1 = SDL_StringToNative(tooltip))
+            {
+                iSDL_SetTrayTooltip(tray, ptr1);
+            }
         }
 
         public static SDL_TrayMenu* SDL_CreateTrayMenu(SDL_Tray* tray)
@@ -45,9 +51,9 @@ namespace Engine.SDL3
             return iSDL_GetTraySubmenu(entry);
         }
 
-        public static SDL_TrayEntry** SDL_GetTrayEntries(SDL_TrayMenu* menu, int* count)
+        public static SDL_TrayEntry*[] SDL_GetTrayEntries(SDL_TrayMenu* menu, out int count)
         {
-            return iSDL_GetTrayEntries(menu, count);
+            return SDL_NativeToArray(iSDL_GetTrayEntries(menu, null), out count);
         }
 
         public static void SDL_RemoveTrayEntry(SDL_TrayEntry* entry)
@@ -55,44 +61,50 @@ namespace Engine.SDL3
             iSDL_RemoveTrayEntry(entry);
         }
 
-        public static SDL_TrayEntry* SDL_InsertTrayEntryAt(SDL_TrayMenu* menu, int pos, byte* label, uint flags)
+        public static SDL_TrayEntry* SDL_InsertTrayEntryAt(SDL_TrayMenu* menu, int pos, string label, uint flags)
         {
-            return iSDL_InsertTrayEntryAt(menu, pos, label, flags);
+            fixed (byte* ptr1 = SDL_StringToNative(label))
+            {
+                return iSDL_InsertTrayEntryAt(menu, pos, ptr1, flags);
+            }
         }
 
-        public static void SDL_SetTrayEntryLabel(SDL_TrayEntry* entry, byte* label)
+        public static void SDL_SetTrayEntryLabel(SDL_TrayEntry* entry, string label)
         {
-            iSDL_SetTrayEntryLabel(entry, label);
+            fixed (byte* ptr1 = SDL_StringToNative(label))
+            {
+                iSDL_SetTrayEntryLabel(entry, ptr1);
+            }
         }
 
-        public static byte* SDL_GetTrayEntryLabel(SDL_TrayEntry* entry)
+        public static string SDL_GetTrayEntryLabel(SDL_TrayEntry* entry)
         {
-            return iSDL_GetTrayEntryLabel(entry);
+            return SDL_NativeToString(iSDL_GetTrayEntryLabel(entry));
         }
 
-        public static void SDL_SetTrayEntryChecked(SDL_TrayEntry* entry, SDL_Bool @checked)
+        public static void SDL_SetTrayEntryChecked(SDL_TrayEntry* entry, bool @checked)
         {
             iSDL_SetTrayEntryChecked(entry, @checked);
         }
 
-        public static SDL_Bool SDL_GetTrayEntryChecked(SDL_TrayEntry* entry)
+        public static bool SDL_GetTrayEntryChecked(SDL_TrayEntry* entry)
         {
             return iSDL_GetTrayEntryChecked(entry);
         }
 
-        public static void SDL_SetTrayEntryEnabled(SDL_TrayEntry* entry, SDL_Bool enabled)
+        public static void SDL_SetTrayEntryEnabled(SDL_TrayEntry* entry, bool enabled)
         {
             iSDL_SetTrayEntryEnabled(entry, enabled);
         }
 
-        public static SDL_Bool SDL_GetTrayEntryEnabled(SDL_TrayEntry* entry)
+        public static bool SDL_GetTrayEntryEnabled(SDL_TrayEntry* entry)
         {
             return iSDL_GetTrayEntryEnabled(entry);
         }
 
-        public static void SDL_SetTrayEntryCallback(SDL_TrayEntry* entry, IntPtr callback, IntPtr userdata)
+        public static void SDL_SetTrayEntryCallback(SDL_TrayEntry* entry, SDL_TrayCallback callback, IntPtr userdata)
         {
-            iSDL_SetTrayEntryCallback(entry, callback, userdata);
+            iSDL_SetTrayEntryCallback(entry, Marshal.GetFunctionPointerForDelegate(callback), userdata);
         }
 
         public static void SDL_ClickTrayEntry(SDL_TrayEntry* entry)
