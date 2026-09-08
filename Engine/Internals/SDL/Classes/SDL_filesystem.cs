adb8a06be0dcaa5f825956b0926b26d42d93a19a
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-08 14:18:29 +0100

    Update SDL_filesystem.cs

--- Engine/Internals/SDL/Classes/SDL_filesystem.cs
+++ Engine/Internals/SDL/Classes/SDL_filesystem.cs
@@ -5,59 +5,88 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static byte* SDL_GetBasePath()
+        public static string SDL_GetBasePath()
         {
-            return iSDL_GetBasePath();
+            return SDL_NativeToString(iSDL_GetBasePath());
         }
 
-        public static byte* SDL_GetPrefPath(byte* org, byte* app)
+        public static string SDL_GetPrefPath(string org, string app)
         {
-            return iSDL_GetPrefPath(org, app);
+            fixed (byte* ptr1 = SDL_StringToNative(org))
+            fixed (byte* ptr2 = SDL_StringToNative(app))
+            {
+                return SDL_NativeToString(iSDL_GetPrefPath(ptr1, ptr2));
+            }
         }
 
-        public static byte* SDL_GetUserFolder(SDL_Folder folder)
+        public static string SDL_GetUserFolder(SDL_Folder folder)
         {
-            return iSDL_GetUserFolder(folder);
+            return SDL_NativeToString(iSDL_GetUserFolder(folder));
         }
 
-        public static SDL_Bool SDL_CreateDirectory(byte* path)
+        public static bool SDL_CreateDirectory(string path)
         {
-            return iSDL_CreateDirectory(path);
+            fixed (byte* ptr1 = SDL_StringToNative(path))
+            {
+                return iSDL_CreateDirectory(ptr1);
+            }
         }
 
-        public static SDL_Bool SDL_EnumerateDirectory(byte* path, IntPtr callback, IntPtr userdata)
+        public static bool SDL_EnumerateDirectory(string path, SDL_EnumerateDirectoryCallback callback, IntPtr userdata)
         {
-            return iSDL_EnumerateDirectory(path, callback, userdata);
+            fixed (byte* ptr1 = SDL_StringToNative(path))
+            {
+                return iSDL_EnumerateDirectory(ptr1, Marshal.GetFunctionPointerForDelegate(callback), userdata);
+            }
         }
 
-        public static SDL_Bool SDL_RemovePath(byte* path)
+        public static bool SDL_RemovePath(string path)
         {
-            return iSDL_RemovePath(path);
+            fixed (byte* ptr1 = SDL_StringToNative(path))
+            {
+                return iSDL_RemovePath(ptr1);
+            }
         }
 
-        public static SDL_Bool SDL_RenamePath(byte* oldpath, byte* newpath)
+        public static bool SDL_RenamePath(string oldpath, string newpath)
         {
-            return iSDL_RenamePath(oldpath, newpath);
+            fixed (byte* ptr1 = SDL_StringToNative(oldpath))
+            fixed (byte* ptr2 = SDL_StringToNative(newpath))
+            {
+                return iSDL_RenamePath(ptr1, ptr2);
+            }
         }
 
-        public static SDL_Bool SDL_CopyFile(byte* oldpath, byte* newpath)
+        public static bool SDL_CopyFile(string oldpath, string newpath)
         {
-            return iSDL_CopyFile(oldpath, newpath);
+            fixed (byte* ptr1 = SDL_StringToNative(oldpath))
+            fixed (byte* ptr2 = SDL_StringToNative(newpath))
+            {
+                return iSDL_CopyFile(ptr1, ptr2);
+            }
         }
 
-        public static SDL_Bool SDL_GetPathInfo(byte* path, SDL_PathInfo* info)
+        public static bool SDL_GetPathInfo(string path, out SDL_PathInfo info)
         {
-            return iSDL_GetPathInfo(path, info);
+            fixed (byte* ptr1 = SDL_StringToNative(path))
+            fixed (SDL_PathInfo* ptr2 = &info)
+            {
+                return iSDL_GetPathInfo(ptr1, ptr2);
+            }
         }
 
-        public static byte** SDL_GlobDirectory(byte* path, byte* pattern, uint flags, int* count)
+        public static string[] SDL_GlobDirectory(string path, string pattern, uint flags, out int count)
         {
-            return iSDL_GlobDirectory(path, pattern, flags, count);
+            fixed (byte* ptr1 = SDL_StringToNative(path))
+            fixed (byte* ptr2 = SDL_StringToNative(pattern))
+            {
+                return SDL_NativeToStringArray(iSDL_GlobDirectory(ptr1, ptr2, flags, null), out count);
+            }
         }
 
-        public static byte* SDL_GetCurrentDirectory()
+        public static string SDL_GetCurrentDirectory()
         {
-            return iSDL_GetCurrentDirectory();
+            return SDL_NativeToString(iSDL_GetCurrentDirectory());
         }
     }
 }
