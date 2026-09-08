ad42afe0ff183623bb3a0547d1f8c9950d6faa16
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-09-08 15:17:55 +0100

    Update SDL_loadso.cs

--- Engine/Internals/SDL/Classes/SDL_loadso.cs
+++ Engine/Internals/SDL/Classes/SDL_loadso.cs
@@ -5,14 +5,20 @@ namespace Engine.SDL3
 {
     internal static unsafe partial class SDL
     {
-        public static SDL_SharedObject* SDL_LoadObject(byte* sofile)
+        public static SDL_SharedObject* SDL_LoadObject(string sofile)
         {
-            return iSDL_LoadObject(sofile);
+            fixed (byte* ptr1 = SDL_StringToNative(sofile))
+            {
+                return iSDL_LoadObject(ptr1);
+            }
         }
 
-        public static IntPtr SDL_LoadFunction(SDL_SharedObject* handle, byte* name)
+        public static IntPtr SDL_LoadFunction(SDL_SharedObject* handle, string name)
         {
-            return iSDL_LoadFunction(handle, name);
+            fixed (byte* ptr1 = SDL_StringToNative(name))
+            {
+                return iSDL_LoadFunction(handle, ptr1);
+            }
         }
 
         public static void SDL_UnloadObject(SDL_SharedObject* handle)
